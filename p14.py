ee15dfa126c29445d084df7c2a2c1c24ca1c4efd
Anu Ann Biju <anuannbiju06@gmail.com>
2026-09-08 13:50:53 +0530

    Fix digit sum calculation for 5 digit numbers
    Refactor digit sum calculation to include digit count validation.

--- p14.py
+++ p14.py
@@ -1,10 +1,15 @@
 #Sum of a 5 digit number
 n=int(input("Enter a 5 digit number: "))
-if 10000<=n<=99999:
-    sum=0
+temp=n
+count=0
+    while temp:
+        c+=1
+        t=t//10
+sum=0
+if count==5:
     while n:
         digit=n%10
-        sum+=digit
+        sum=sum+digit
         n=n//10
     print("Sum of digits:", sum)
 else:
