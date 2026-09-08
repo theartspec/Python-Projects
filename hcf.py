0a191a9f164396c928a3036e5bc393d9a07195ad
Anu Ann Biju <anuannbiju06@gmail.com>
2026-09-08 14:58:34 +0530

    Enhance hcf.py to compute HCF and LCM
    Updated the script to calculate both HCF and LCM of two numbers instead of just HCF.

--- hcf.py
+++ hcf.py
@@ -1,6 +1,10 @@
-#to find the hcf of two numbers
-a,b=map(int,input("Enter two numbers: ").split())
-for i in range(1,a+1):
+#to find the hcf and lcm of two numbers
+a=int(input("Enter a number: "))
+b=int(input("Enter a number: "))
+for i in range(1,min(a,b)+1):
     if a%i==0 and b%i==0:
-        hcf=i
-print("HCF of two numbers is:",hcf)
+        hcf = i
+print ("HCF/GCD = " ,hcf) 
+lcm=(a*b)//hcf
+print("LCM = ",lcm)
+        
