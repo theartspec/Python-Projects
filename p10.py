89ab947188f9d8e9566bbe775da2568276dd668a
Anu Ann Biju <anuannbiju06@gmail.com>
2026-09-08 12:01:54 +0530

    Add functionality to reverse a number

--- p10.py
+++ p10.py
@@ -1,1 +1,8 @@
+#Reverse of a number
 n=int(input("Enter a number: "))
+rev=0
+while n:                # runs until n is not equal to zero
+    digit=n%10
+    rev=rev*10+digit
+    n=n//10
+print("Reverse of the number is",rev)
